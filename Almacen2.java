3b5efdfa6c5ccca6a5d67ab8cb8a0bf36122285c
Nheiel <142445622+Nheiel@users.noreply.github.com>
2023-11-05 15:50:19 -0600

    Add files via upload

---
+++ Almacen2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.almacen2;
+
+/**
+ *
+ * @author Alessandro
+ */
+public class Almacen2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
