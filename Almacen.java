c45071a2ddb1ba69df89a9d7a4b405f8f62f621a
RodrigoHinostroza <142445346+RodrigoHinostroza@users.noreply.github.com>
2023-11-05 20:28:18 -0500

    Update Almacen.java

--- Almacen.java
+++ Almacen.java
@@ -1,24 +1,34 @@
-package com.mycompany.almacen;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Almacen {
     private List<String> inventario;
+    private Map<String, String> productoSectorMap;
 
     public Almacen() {
         inventario = new ArrayList<>();
+        productoSectorMap = new HashMap<>();
     }
 
-    public void agregarProducto(String producto) {
-        inventario.add(producto);
-        System.out.println("Producto agregado al almacén: " + producto);
+    public void agregarProducto(String producto, String sector) {
+        if ("comida".equalsIgnoreCase(sector) || "útiles de casa".equalsIgnoreCase(sector)) {
+            inventario.add(producto);
+            productoSectorMap.put(producto, sector);
+            System.out.println("Producto agregado al almacén en el sector: " + sector);
+        } else {
+            System.out.println("El sector especificado no es válido.");
+        }
     }
 
     public void quitarProducto(String producto) {
         if (inventario.contains(producto)) {
+            String sector = productoSectorMap.get(producto);
             inventario.remove(producto);
-            System.out.println("Producto eliminado del almacén: " + producto);
+            productoSectorMap.remove(producto);
+            System.out.println("Producto eliminado del almacén en el sector: " + sector);
         } else {
             System.out.println("Producto no encontrado en el almacén: " + producto);
         }
@@ -30,32 +40,44 @@ public class Almacen {
         } else {
             System.out.println("Inventario del almacén:");
             for (String producto : inventario) {
-                System.out.println(producto);
+                String sector = productoSectorMap.get(producto);
+                System.out.println(producto + " (Sector: " + sector + ")");
             }
+        }
+    }
+
+    public String buscarProducto(String producto) {
+        if (productoSectorMap.containsKey(producto)) {
+            return productoSectorMap.get(producto);
+        } else {
+            return "Producto no encontrado en el almacén.";
         }
     }
 
     public static void main(String[] args) {
         Almacen almacen = new Almacen();
         Scanner scanner = new Scanner(System.in);
-        
+
         int opcion;
         do {
             System.out.println("Menú de opciones:");
             System.out.println("1. Agregar producto");
             System.out.println("2. Eliminar producto");
             System.out.println("3. Imprimir inventario");
-            System.out.println("4. Salir");
+            System.out.println("4. Buscar producto por nombre");
+            System.out.println("5. Salir");
             System.out.print("Seleccione una opción: ");
-            
+
             opcion = scanner.nextInt();
             scanner.nextLine(); // Consumir la línea en blanco
-            
+
             switch (opcion) {
                 case 1:
                     System.out.print("Ingrese el nombre del producto a agregar: ");
                     String producto = scanner.nextLine();
-                    almacen.agregarProducto(producto);
+                    System.out.print("Ingrese el sector del producto: ");
+                    String sector = scanner.nextLine();
+                    almacen.agregarProducto(producto, sector);
                     break;
                 case 2:
                     System.out.print("Ingrese el nombre del producto a eliminar: ");
@@ -66,12 +88,19 @@ public class Almacen {
                     almacen.imprimirInventario();
                     break;
                 case 4:
+                    System.out.print("Ingrese el nombre del producto a buscar: ");
+                    producto = scanner.nextLine();
+                    String sectorEncontrado = almacen.buscarProducto(producto);
+                    System.out.println("El producto se encuentra en el sector: " + sectorEncontrado);
+                    break;
+                case 5:
                     System.out.println("Saliendo del programa.");
                     break;
                 default:
                     System.out.println("Opción no válida. Intente de nuevo.");
             }
-        } while (opcion != 4);
+        } while (opcion != 5);
     }
 }
+
 
